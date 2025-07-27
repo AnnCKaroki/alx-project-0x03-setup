5f43ec51559fb802b0ec2a55c5440b1bf526d4ca
Ann <annkarokiadmissions@gmail.com>
2025-07-27 18:09:41 +0300

    Fix useRouter

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -6,7 +6,7 @@ interface PageRouteProps {
 }
 
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
 
   // Imeperative routing with useRouter
   const routeToNextPage  = ({ pageRoute }: PageRouteProps) => {
@@ -33,4 +33,3 @@ export default function Home() {
     </div>
   );
 }
-
